8d967a88ea1ba502dde1ef5858e1e3f61211684b
R1verb <128699019+R1verb@users.noreply.github.com>
2024-01-30 17:27:10 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ const scroll = document.querySelector('.scroll');
 const cart = document.querySelector('.cart');
 const mobileMenuLinks = document.querySelectorAll('.nav_list_mobile .nav_link');
 
+const reproductButtons = document.querySelectorAll('.reproduct_button');
+const reproductBlocks = document.querySelectorAll('.reproduct_block');
+
+
 mobileMenu.addEventListener('click', () => {
     mobileMenu.classList.toggle('mobile_menu-active');
     navList.classList.toggle('nav_list_mobile-active');
@@ -30,3 +34,22 @@ const closeMobileMenu = () => {
 };
 
 window.addEventListener('resize', closeMobileMenu);
+
+
+
+reproductButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        reproductButtons.forEach(btn => btn.classList.remove('reproduct_button-active'));
+        button.classList.add('reproduct_button-active');
+        
+        const targetId = button.id + '-block';
+
+        reproductBlocks.forEach(block => {
+            if (block.id === targetId) {
+                block.classList.add('reproduct_block-active');
+            } else {
+                block.classList.remove('reproduct_block-active');
+            }
+        });
+    });
+});
