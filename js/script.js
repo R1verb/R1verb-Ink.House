9f01f0c7489aad83360d5a706478f182b2dc742a
R1verb <128699019+R1verb@users.noreply.github.com>
2024-01-30 12:19:56 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 const mobileMenu = document.querySelector('.mobile_menu');
 const navList = document.querySelector('.nav_list_mobile');
 const scroll = document.querySelector('.scroll');
+const mobileMenuLinks = document.querySelectorAll('.nav_list_mobile .nav_link');
 
-mobileMenu.addEventListener('click', ()=>{
+mobileMenu.addEventListener('click', () => {
     mobileMenu.classList.toggle('mobile_menu-active');
     navList.classList.toggle('nav_list_mobile-active');
     scroll.classList.toggle('scroll-active');
+});
+
+mobileMenuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        mobileMenu.classList.remove('mobile_menu-active');
+        navList.classList.remove('nav_list_mobile-active');
+        scroll.classList.remove('scroll-active');
+    });
 });
 
 const closeMobileMenu = () => {
@@ -15,7 +24,5 @@ const closeMobileMenu = () => {
         scroll.classList.remove('scroll-active');
     }
 };
-
-
 
 window.addEventListener('resize', closeMobileMenu);
